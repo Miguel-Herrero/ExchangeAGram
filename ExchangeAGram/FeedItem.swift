034e20a9394f7d9ed2fff2b561aab4926477c5fb
Miguel Herrero <miguel.herrerobaena@gmail.com>
2015-02-06 13:38:07 +0100

    Creating a FeedItem

---
+++ ExchangeAGram/FeedItem.swift
@@ -0,0 +1,19 @@
+//
+//  FeedItem.swift
+//  ExchangeAGram
+//
+//  Created by Miguel Herrero on 6/2/15.
+//  Copyright (c) 2015 Miguel Herrero. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+// Add Objective-C compatibility
+@objc (FeedItem)
+class FeedItem: NSManagedObject {
+
+    @NSManaged var caption: String
+    @NSManaged var image: NSData
+
+}
