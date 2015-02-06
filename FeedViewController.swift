639e78c012ea0fdc309faab48ee12db5a7f3ff66
Miguel Herrero <miguel.herrerobaena@gmail.com>
2015-02-06 13:55:10 +0100

    NSFetchRequest

--- FeedViewController.swift
+++ FeedViewController.swift
@@ -13,11 +13,20 @@ import CoreData
 class FeedViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    var feedArray: [AnyObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let request = NSFetchRequest(entityName: "FeedItem")
+        let appDelegate: AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext!
+        
+        // Make the request
+        feedArray = context.executeFetchRequest(request, error: nil)!
     }
 
     override func didReceiveMemoryWarning() {
