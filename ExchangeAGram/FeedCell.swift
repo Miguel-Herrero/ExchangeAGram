a46434f730810028f86fd7760f34498a25a41c54
Miguel Herrero <miguel.herrerobaena@gmail.com>
2015-02-06 12:40:50 +0100

    FeedCell

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Miguel Herrero on 6/2/15.
+//  Copyright (c) 2015 Miguel Herrero. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
