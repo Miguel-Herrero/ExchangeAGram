26af0e2605ba2b9360bba174fe4ab3df10f14666
Miguel Herrero <miguel.herrerobaena@gmail.com>
2015-02-07 22:36:27 +0100

    Bridging Header

---
+++ Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Miguel Herrero on 7/2/15.
+//  Copyright (c) 2015 Miguel Herrero. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
